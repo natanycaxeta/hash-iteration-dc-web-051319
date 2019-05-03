49ddfa8032c9c9404a3398411fa0dce4bbbc5d87
Natany Caxeta <natanycaxeta@icloud.com>
2019-05-03 19:16:20 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,7 +9,7 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
-
+binding'pry'
 def age_appropriate_birthday(birthday_kids)
   birthday_kids = {
       "Timmy" => 9,
