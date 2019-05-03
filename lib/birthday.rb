ae4161446a356257b64e2ec9e7c12cc1564b62df
Natany Caxeta <natanycaxeta@icloud.com>
2019-05-03 19:19:35 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -18,9 +18,9 @@ def age_appropriate_birthday(birthday_kids)
       "Amanda" => 27
     }
   birthday_kids.each do |kids_name, age|
- # if age < 12
+  if age < 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
- # else
+  else
     puts "You are too old for this."
   end
 end
